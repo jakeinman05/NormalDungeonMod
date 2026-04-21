7e46a774296bdc810b35a3d9907cb83a8be48de6
jakeinman05 <jakeinman05@gmail.com>
2026-04-21 15:57:29 -0400

    added hurt particle

--- src/main/java/net/poob22/normaldm/common/client/particles/NDMParticles.java
+++ src/main/java/net/poob22/normaldm/common/client/particles/NDMParticles.java
@@ -13,6 +13,7 @@ public class NDMParticles {
     public static final DeferredRegister<ParticleType<?>> DEF_REG = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, MODID);
 
     public static final RegistryObject<SimpleParticleType> BLOOD_POOL = DEF_REG.register("blood_pool", () -> new SimpleParticleType(false));
+    public static final RegistryObject<SimpleParticleType> HURT_PARTICLE = DEF_REG.register("hurt_particle", () -> new SimpleParticleType(false));
 
     public static void register(IEventBus bus) {
         DEF_REG.register(bus);
