fb9111653d397b6b77aacc62db30873cc8c63993
jakeinman05 <jakeinman05@gmail.com>
2026-04-21 18:03:14 -0400

    Merged EntityAttributesEvents.java into DungeonMobs.java

--- src/main/java/net/poob22/normaldm/common/server/entity/registry/DungeonMobs.java
+++ src/main/java/net/poob22/normaldm/common/server/entity/registry/DungeonMobs.java
@@ -2,7 +2,12 @@ package net.poob22.normaldm.common.server.entity.registry;
 
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.poob22.normaldm.common.client.model.FleshBlobModel;
 import net.poob22.normaldm.common.client.model.FleshGuyModel;
 import net.poob22.normaldm.common.client.model.maggot.ChargerMaggotModel;
@@ -19,6 +24,7 @@ import net.poob22.normaldm.common.server.entity.living.MaggotEntity;
 
 import static net.poob22.normaldm.NormalDungeonMod.MODID;
 
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DungeonMobs {
     public static void init() {}
 
@@ -32,4 +38,13 @@ public class DungeonMobs {
     public static final DungeonMobDefinition<FleshGuyEntity> FLESH_GUY = DungeonMobRegistry.register(new DungeonMobDefinition<>("flesh_guy", FleshGuyEntity::new, MobCategory.MONSTER, 0.6F, 1.5F, FleshGuyEntity::createAttributes, FLESH_GUY_LAYER, FleshGuyModel::createBodyLayer, FleshGuyRenderer::new));
     public static final DungeonMobDefinition<FleshBlobEntity> FLESH_BLOB = DungeonMobRegistry.register(new DungeonMobDefinition<>("flesh_blob", FleshBlobEntity::new, MobCategory.MONSTER, 0.6F, 0.5F, FleshBlobEntity::createAttributes, FLESH_BLOB_LAYER, FleshBlobModel::createBodyLayer, FleshBlobRenderer::new));
     public static final DungeonMobDefinition<MaggotEntity> MAGGOT = DungeonMobRegistry.register(new DungeonMobDefinition<>("maggot", MaggotEntity::new, MobCategory.MONSTER, 0.4F, 0.3F, MaggotEntity::createAttributes, MAGGOT_LAYER, MaggotModel::createBodyLayer, MaggotRenderer::new));
+
+
+    @SubscribeEvent
+    public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
+        for(DungeonMobDefinition<?> def : DungeonMobRegistry.MOBS) {
+            event.put((EntityType<? extends LivingEntity>) NDMEntities.get(def.id), def.attributes.get().build());
+        }
+    }
+
 }
