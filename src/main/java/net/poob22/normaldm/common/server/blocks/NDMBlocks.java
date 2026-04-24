912be79ee472cb3a0f6f2d6324ed67d99335c64d
jakeinman05 <jakeinman05@gmail.com>
2026-04-24 17:22:43 -0400

    Added 9 cellar blocks & a flesh block for fun.

--- src/main/java/net/poob22/normaldm/common/server/blocks/NDMBlocks.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/NDMBlocks.java
@@ -4,6 +4,8 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,7 +18,18 @@ import java.util.function.Supplier;
 public class NDMBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NormalDungeonMod.MODID);
 
-    public static final RegistryObject<Block> ROOM_CONTROLLER_BLOCK = registerBlockWithItem("room_controller", () -> new RoomControllerBlock());
+    public static final RegistryObject<Block> ROOM_CONTROLLER_BLOCK = registerBlockWithItem("room_controller", RoomControllerBlock::new);
+    public static final RegistryObject<Block> PLYWOOD = registerBlockWithItem("plywood", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
+    public static final RegistryObject<Block> CELLAR_WALL = registerBlockWithItem("cellar_wall", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_WALL_DRAIN = registerBlockWithItem("cellar_wall_drain", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_WALL_CRACKED = registerBlockWithItem("cellar_wall_cracked", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_WALL_MOSSY = registerBlockWithItem("cellar_wall_mossy", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_CEILING = registerBlockWithItem("cellar_ceiling", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_CEILING_RIPPED = registerBlockWithItem("cellar_ceiling_ripped", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_CEILING_MOLDY = registerBlockWithItem("cellar_ceiling_moldy", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> CELLAR_CEILING_FLESH_TORN = registerBlockWithItem("cellar_ceiling_flesh_torn", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> FLESH_BLOCK = registerBlockWithItem("flesh_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.SCULK)));
+
 
     private static <T extends Block> RegistryObject<T> registerBlockWithItem(String name, Supplier<T> block) {
         RegistryObject<T> blockRegistry = BLOCKS.register(name, block);
