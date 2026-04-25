b10984ca1c864941e8766bc97fd486fae52e1e45
jakeinman05 <jakeinman05@gmail.com>
2026-04-25 13:14:49 -0400

    changed way type is grabbed from spawn eggs

--- src/main/java/net/poob22/normaldm/common/server/items/DungeonMobSpawnEgg.java
+++ src/main/java/net/poob22/normaldm/common/server/items/DungeonMobSpawnEgg.java
@@ -2,12 +2,20 @@ package net.poob22.normaldm.common.server.items;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.poob22.normaldm.NormalDungeonMod;
 import net.poob22.normaldm.common.server.entity.living.DungeonMob;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
 public class DungeonMobSpawnEgg extends ForgeSpawnEggItem {
+
     public DungeonMobSpawnEgg(Supplier<? extends EntityType<? extends DungeonMob>> type, int backgroundColor, int highlightColor, Properties props) {
         super(type, backgroundColor, highlightColor, props);
+    }
+
+    @SuppressWarnings("unchecked")
+    public EntityType<DungeonMob> getType() {
+        return (EntityType<DungeonMob>) this.getDefaultType();
     }
 }
