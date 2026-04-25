79bf41f1594d63c06874b6f05a76da0a84a2eb1f
jakeinman05 <jakeinman05@gmail.com>
2026-04-25 13:15:14 -0400

    added interactionResult return to cancel use event

--- src/main/java/net/poob22/normaldm/common/server/blocks/DungeonMobSpawnerBlock.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/DungeonMobSpawnerBlock.java
@@ -1,8 +1,12 @@
 package net.poob22.normaldm.common.server.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -15,7 +19,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.poob22.normaldm.NormalDungeonMod;
 import net.poob22.normaldm.common.server.blocks.blockentities.DungeonMobSpawner;
+import net.poob22.normaldm.common.server.entity.living.DungeonMob;
+import net.poob22.normaldm.common.server.items.DungeonMobSpawnEgg;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,11 +39,23 @@ public class DungeonMobSpawnerBlock extends BaseEntityBlock {
     @Override
     public InteractionResult use(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand interactionHand, @NotNull BlockHitResult blockHitResult) {
         if(!level.isClientSide) {
+            NormalDungeonMod.LOGGER.info("use attempt");
             BlockEntity blockEntity = level.getBlockEntity(pos);
             ItemStack handItem = player.getItemInHand(interactionHand);
             if(blockEntity instanceof DungeonMobSpawner spawner && !player.isShiftKeyDown()) {
-                //if(!handItem.isEmpty() && handItem) {}
+                if(!handItem.isEmpty() && handItem.getItem() instanceof DungeonMobSpawnEgg egg) {
+                    EntityType<DungeonMob> type = egg.getType();
+                    NormalDungeonMod.LOGGER.info("type: " + type);
+                    if(type != null) {
+                        spawner.setMobToSpawn(type);
+                        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_HIT_PLAYER, SoundSource.PLAYERS, 1.0F, 1.0F);
+                        player.sendSystemMessage(Component.literal("Spawner has been set to spawn: " + type));
+                    }
+                } else if(handItem.isEmpty()) {
+                    player.sendSystemMessage(Component.literal("Spawner has been set to spawn: " + spawner.getMobToSpawn()));
+                }
             }
+            return InteractionResult.SUCCESS;
         }
 
         return super.use(state, level, pos, player, interactionHand, blockHitResult);
