c9f60e6741ec4c80dfe66fb7cb4a768bb795d60e
jakeinman05 <jakeinman05@gmail.com>
2026-04-27 14:21:23 -0400

    Added creative mode tab.

--- src/main/java/net/poob22/normaldm/NormalDungeonMod.java
+++ src/main/java/net/poob22/normaldm/NormalDungeonMod.java
@@ -1,6 +1,10 @@
 package net.poob22.normaldm;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,7 +15,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.poob22.normaldm.common.client.packet.PacketHandler;
 import net.poob22.normaldm.common.client.particles.NDMParticles;
 import net.poob22.normaldm.common.server.blocks.NDMBlocks;
@@ -27,22 +33,46 @@ public class NormalDungeonMod
     public static final String MODID = "normaldm";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NormalDungeonMod.MODID);
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_MODE_TAB.register("normaldm_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(NDMBlocks.CELLAR_WALL.get()))
+            .title(Component.literal("Normal Dungeon Mod"))
+            .displayItems(((parameters, output) -> {
+                output.accept(NDMBlocks.CELLAR_WALL.get());
+                output.accept(NDMBlocks.CELLAR_WALL_DRAIN.get());
+                output.accept(NDMBlocks.CELLAR_WALL_CRACKED.get());
+                output.accept(NDMBlocks.CELLAR_WALL_MOSSY.get());
+                output.accept(NDMBlocks.CELLAR_CEILING.get());
+                output.accept(NDMBlocks.CELLAR_CEILING_RIPPED.get());
+                output.accept(NDMBlocks.CELLAR_CEILING_MOLDY.get());
+                output.accept(NDMBlocks.CELLAR_CEILING_FLESH_TORN.get());
+                output.accept(NDMBlocks.PLYWOOD.get());
+                output.accept(NDMBlocks.FLESH_BLOCK.get());
+                output.accept(NDMBlocks.ROOM_CONTROLLER_BLOCK.get());
+                output.accept(NDMBlocks.DUNGEON_MOB_SPAWNER_BLOCK.get());
+                output.accept(NDMItems.MAGGOT_SPAWN_EGG.get());
+                output.accept(NDMItems.CHARGER_MAGGOT_SPAWN_EGG.get());
+                output.accept(NDMItems.FLESH_GUY_SPAWN_EGG.get());
+            }))
+            .build());
+
     public NormalDungeonMod(FMLJavaModLoadingContext context)
     {
-        IEventBus modEventBus = context.getModEventBus();
+        IEventBus bus = context.getModEventBus();
 
         DungeonMobs.init();
         NDMEntities.RegisterAll();
-        NDMEntities.register(modEventBus);
-        NDMBlocks.register(modEventBus);
-        NDMItems.register(modEventBus);
-        NDMBlockEntities.register(modEventBus);
-        NDMParticles.register(modEventBus);
+        NDMEntities.register(bus);
+        NDMBlocks.register(bus);
+        NDMItems.register(bus);
+        NDMBlockEntities.register(bus);
+        NDMParticles.register(bus);
         PacketHandler.registerPackets();
+        CREATIVE_MODE_TAB.register(bus);
 
-        modEventBus.addListener(this::commonSetup);
+        bus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
+        bus.addListener(this::addCreative);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -61,10 +91,8 @@ public class NormalDungeonMod
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {}
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {}
 }
