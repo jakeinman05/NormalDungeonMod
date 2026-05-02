d2cc1e5d757e8c694e3b38708755292af9149edb
jakeinman05 <jakeinman05@gmail.com>
2026-05-02 15:21:34 -0400

    createRoomBounds -> getVolumes

--- src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomDefinition.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomDefinition.java
@@ -1,10 +1,7 @@
 package net.poob22.normaldm.common.server.blocks.properties;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.AABB;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RoomDefinition {
@@ -20,35 +17,8 @@ public class RoomDefinition {
         this.volumes = List.of(volumes);
     }
 
-    public List<AABB> createRoomBounds(BlockPos origin, boolean playerBounds) {
-        if(!playerBounds){
-            List<AABB> roomBounds = new ArrayList<>();
-            for(RoomVolume v : this.volumes){
-                roomBounds.add(v.toAABB(origin));
-            }
-            return roomBounds;
-        }
-        List<AABB> playerBoxes = new ArrayList<>();
-        for(RoomVolume v : this.volumes){
-            playerBoxes.add(v.toAABB(origin).deflate(1.25D));
-        }
-        return playerBoxes;
-    }
-
-    public List<BlockPos> getMinOfVolumes() {
-        List<BlockPos> pos = new ArrayList<>();
-        for(RoomVolume v : this.volumes){
-            pos.add(v.min);
-        }
-        return pos;
-    }
-
-    public List<BlockPos> getMaxOfVolumes() {
-        List<BlockPos> pos = new ArrayList<>();
-        for(RoomVolume v : this.volumes){
-            pos.add(v.max);
-        }
-        return pos;
+    public List<RoomVolume> getVolumes() {
+        return this.volumes;
     }
 
     public String toString() {
