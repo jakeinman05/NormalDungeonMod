f4353104cc07d74864f3cf8a8e88badc8bfc7211
jakeinman05 <jakeinman05@gmail.com>
2026-05-02 15:21:55 -0400

    all existing room types now implement volumes

--- src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomDefinitions.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomDefinitions.java
@@ -1,9 +1,7 @@
 package net.poob22.normaldm.common.server.blocks.properties;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.AABB;
 import net.poob22.normaldm.NormalDungeonMod;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
@@ -14,43 +12,39 @@ public class RoomDefinitions {
             new RoomDefinition(
                     "small",
                     RoomType.SMALL,
-                    new RoomVolume(new BlockPos(-5, 1, -5), new BlockPos(5, 7, 5))
+                    new RoomVolume(new BlockPos(-5, 1, -5), new BlockPos(6, 7, 6))
             )
     );
 
     public static final RoomDefinition SMALL_HALLWAY_NS = register(
             new RoomDefinition(
                 "small_hallway_ns",
-                    new BlockPos(-3, 1, -6),
-                    new BlockPos(3, 7, 6),
-                    RoomType.SMALL_HALLWAY
+                    RoomType.SMALL_HALLWAY,
+                    new RoomVolume(new BlockPos(-3, 1, -6), new BlockPos(4, 7, 7))
             )
     );
 
     public static final RoomDefinition SMALL_HALLWAY_EW = register(
             new RoomDefinition(
                     "small_hallway_ew",
-                    new BlockPos(-6, 1, -3),
-                    new BlockPos(6, 7, 3),
-                    RoomType.SMALL_HALLWAY
+                    RoomType.SMALL_HALLWAY,
+                    new RoomVolume(new BlockPos(-6, 1, -3), new BlockPos(7, 7, 4))
             )
     );
 
     public static final RoomDefinition MEDIUM = register(
             new RoomDefinition(
                     "medium",
-                    new BlockPos(-8, 1, -8),
-                    new BlockPos(8, 7, 8),
-                    RoomType.MEDIUM
+                    RoomType.MEDIUM,
+                    new RoomVolume(new BlockPos(-8, 1, -8), new BlockPos(9, 7, 9))
             )
     );
 
     public static final RoomDefinition LARGE = register(
             new RoomDefinition(
                     "large",
-                    new BlockPos(-10, 1, -10),
-                    new BlockPos(10, 8, 10),
-                    RoomType.LARGE
+                    RoomType.LARGE,
+                    new RoomVolume(new BlockPos(-10, 1, -10), new BlockPos(11, 8, 11))
             )
     );
 
