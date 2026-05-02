affff84599c752cd9182736691a01bb4710f8bd7
jakeinman05 <jakeinman05@gmail.com>
2026-05-02 15:22:30 -0400

    toAABB now returns correct room bounding box representing volume

--- src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomVolume.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomVolume.java
@@ -4,15 +4,27 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.AABB;
 
 public class RoomVolume {
-    BlockPos min;
-    BlockPos max;
+    public final BlockPos min;
+    public final BlockPos max;
 
     public RoomVolume(BlockPos min, BlockPos max) {
         this.min = min;
         this.max = max;
     }
 
-    public AABB toAABB(BlockPos origin) {
-        return new AABB(origin.offset(this.min), origin.offset(this.max));
+    public AABB toAABB(BlockPos origin, boolean isPlayerBound) {
+        BlockPos a = origin.offset(this.min);
+        BlockPos b = origin.offset(this.max);
+
+        AABB box = new AABB(
+                Math.min(a.getX(), b.getX()),
+                Math.min(a.getY(), b.getY()),
+                Math.min(a.getZ(), b.getZ()),
+                Math.max(a.getX(), b.getX()),
+                Math.max(a.getY(), b.getY()),
+                Math.max(a.getZ(), b.getZ())
+        );
+
+        return isPlayerBound ? box.deflate(1.25D) : box;
     }
 }
