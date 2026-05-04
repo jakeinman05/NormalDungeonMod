31ebd7b339f83723f477974404d5a3e315a97d76
jakeinman05 <jakeinman05@gmail.com>
2026-05-04 14:10:39 -0400

    universal dungeon mob follow range

--- src/main/java/net/poob22/normaldm/common/server/entity/living/DungeonMob.java
+++ src/main/java/net/poob22/normaldm/common/server/entity/living/DungeonMob.java
@@ -1,6 +1,5 @@
 package net.poob22.normaldm.common.server.entity.living;
 
-import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -8,6 +7,8 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
@@ -34,6 +35,10 @@ public class DungeonMob extends Monster {
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(IN_DUNGEON, false);
+    }
+
+    public static AttributeSupplier.Builder createDungeonMobAttributes() {
+        return Monster.createMonsterAttributes().add(Attributes.FOLLOW_RANGE, 40.0F);
     }
 
     public void setInDungeon(boolean inDungeon) {
@@ -102,11 +107,11 @@ public class DungeonMob extends Monster {
     public void sendParticles(byte type) {
         AABB box = this.getBoundingBox();
         double offsetX = (box.maxX - box.minX) / 2;
-        double offsetY = (box.maxY - box.minY) / 2;
+        double offsetY = (box.maxY - box.minY) / 3;
         double offsetZ = (box.maxZ - box.minZ) / 2;
         SimpleParticleType particleType = type == 1 ? getDeathParticleType() : getHurtParticleType();
         int amount = type == 1 ? getDeathParticleAmount() : getHurtParticleAmount();
-        ((ServerLevel)this.level()).sendParticles(particleType, this.getX(), this.getY(), this.getZ(), amount, offsetX, offsetY, offsetZ, 0.1D);
+        ((ServerLevel)this.level()).sendParticles(particleType, this.getX(), this.getY() + 0.5, this.getZ(), amount, offsetX, offsetY, offsetZ, 4.0D);
     }
 
     @Override
