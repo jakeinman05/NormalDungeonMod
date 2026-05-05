23c014c8268ac77fb4a33edd9ef76362216efa14
jakeinman05 <jakeinman05@gmail.com>
2026-05-04 20:34:59 -0400

    comment for future use

--- src/main/java/net/poob22/normaldm/common/server/entity/definition/IShootingMob.java
+++ src/main/java/net/poob22/normaldm/common/server/entity/definition/IShootingMob.java
@@ -3,5 +3,6 @@ package net.poob22.normaldm.common.server.entity.definition;
 import net.poob22.normaldm.common.server.entity.projectile.BaseShotEntity;
 
 public interface IShootingMob {
+    /// Make sure any enemy implementing this has its eye level set to when the shot will come out
     BaseShotEntity createProjectile();
 }
