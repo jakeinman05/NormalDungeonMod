1a471657f19440fdda9cbbf50dc5aa3b3ff9b6c1
jakeinman05 <jakeinman05@gmail.com>
2026-05-04 20:35:46 -0400

    Snot shot spews snot particles

--- src/main/java/net/poob22/normaldm/common/server/entity/projectile/SnotShotEntity.java
+++ src/main/java/net/poob22/normaldm/common/server/entity/projectile/SnotShotEntity.java
@@ -8,11 +8,24 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.poob22.normaldm.common.client.particles.NDMParticles;
 import org.jetbrains.annotations.NotNull;
 
 public class SnotShotEntity extends BaseShotEntity {
     public SnotShotEntity(EntityType<? extends Projectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.setHitParticles(NDMParticles.SNOT_PARTICLE.get());
+    }
+
+    @Override
+    public void tick() {
+        if(this.level().isClientSide) {
+            if(this.tickCount % (this.random.nextInt(5) + 5) == 0) {
+                this.level().addParticle(NDMParticles.SNOT_PARTICLE.get(), this.getX(), this.getY(), this.getZ(), -(this.getDeltaMovement().x * 0.3), 0.0D, -(this.getDeltaMovement().z * 0.3));
+            }
+        }
+
+        super.tick();
     }
 
     @Override
