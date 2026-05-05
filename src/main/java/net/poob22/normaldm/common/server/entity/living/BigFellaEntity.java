8696006730452c9c71e0262c3a76abf9f4708081
jakeinman05 <jakeinman05@gmail.com>
2026-05-05 19:14:22 -0400

    uses basic melee goal

--- src/main/java/net/poob22/normaldm/common/server/entity/living/BigFellaEntity.java
+++ src/main/java/net/poob22/normaldm/common/server/entity/living/BigFellaEntity.java
@@ -3,8 +3,11 @@ package net.poob22.normaldm.common.server.entity.living;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.poob22.normaldm.common.server.entity.ai.DungeonMobMeleeGoal;
 
 public class BigFellaEntity extends DungeonMob {
     public BigFellaEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
@@ -14,6 +17,14 @@ public class BigFellaEntity extends DungeonMob {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return DungeonMob.createDungeonMobAttributes().add(Attributes.MAX_HEALTH, 40.0D).add(Attributes.MOVEMENT_SPEED, 0.23D);
+        return DungeonMob.createDungeonMobAttributes().add(Attributes.MAX_HEALTH, 40.0D).add(Attributes.MOVEMENT_SPEED, 0.23D).add(Attributes.ATTACK_DAMAGE, 1).add(Attributes.KNOCKBACK_RESISTANCE, 1.0D);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(0, new DungeonMobMeleeGoal(this));
+
+        this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, Player.class, false));
     }
 }
