e9ae8a590ba0cd88be08f6226b52be54dccb3d56
jakeinman05 <jakeinman05@gmail.com>
2026-05-15 00:18:34 -0400

    RetreatGoal for enemies that will need it

---
+++ src/main/java/net/poob22/normaldm/common/server/entity/ai/RetreatGoal.java
@@ -0,0 +1,51 @@
+package net.poob22.normaldm.common.server.entity.ai;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.util.DefaultRandomPos;
+import net.minecraft.world.phys.Vec3;
+import net.poob22.normaldm.common.server.entity.living.DungeonMob;
+
+public class RetreatGoal extends Goal {
+    DungeonMob mob;
+    protected double retreatDistance;
+    private LivingEntity target;
+
+    public RetreatGoal(DungeonMob mob, double retreatDistance) {
+        this.mob = mob;
+        this.retreatDistance = retreatDistance;
+    }
+
+    @Override
+    public boolean canUse() {
+        this.target = mob.getTarget();
+        return target != null && this.mob.distanceToSqr(target) < this.retreatDistance;
+    }
+
+    @Override
+    public boolean canContinueToUse() {
+        return this.canUse();
+    }
+
+    @Override
+    public void tick() {
+        fleeFromTarget();
+        super.tick();
+    }
+
+    protected void fleeFromTarget() {
+        if(this.target != null && this.target.isAlive()) {
+            Vec3 targetVec;
+            if(mob.distanceTo(this.target) < 1.0) {
+                targetVec = DefaultRandomPos.getPos(this.mob, (int)this.retreatDistance * 2, 2);
+            }
+            else {
+                targetVec = DefaultRandomPos.getPosAway(this.mob, (int) this.retreatDistance + 4, 2, target.position());
+            }
+
+            if(targetVec != null) {
+                this.mob.getNavigation().moveTo(targetVec.x, targetVec.y, targetVec.z, 1.0D);
+            }
+        }
+    }
+}
