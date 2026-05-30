2ff3df2ddf69b37af638a2351428bafb373736c2
jakeinman05 <jakeinman05@gmail.com>
2026-05-30 13:47:59 -0400

    updated room types

--- src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomType.java
+++ src/main/java/net/poob22/normaldm/common/server/blocks/properties/RoomType.java
@@ -4,20 +4,16 @@ import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
 public enum RoomType implements StringRepresentable {
-    SMALL,
+    SQUARE,
     HALLWAY,
-    MEDIUM,
-    LARGE,
     L_SHAPED;
 
 
     @Override
     public @NotNull String getSerializedName() {
         return switch(this) {
-            case SMALL -> "small";
+            case SQUARE -> "square";
             case HALLWAY -> "hallway";
-            case MEDIUM -> "medium";
-            case LARGE -> "large";
             case L_SHAPED -> "l_shaped";
         };
     }
