d43ade451ec69c21828218c7677554cebaf9a265
jakeinman05 <jakeinman05@gmail.com>
2026-06-19 13:00:03 -0400

    initial handler for catching player inputs

--- src/main/java/net/poob22/normaldm/common/server/combat/LeftClickHandler.java
+++ src/main/java/net/poob22/normaldm/common/server/combat/LeftClickHandler.java
@@ -1,5 +1,9 @@
 package net.poob22.normaldm.common.server.combat;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
 public class LeftClickHandler {
     // get player details from left click event
     // get values from player capability
@@ -8,4 +12,9 @@ public class LeftClickHandler {
     // if(cooldown > 0) no attack return
     // else call MainCombatHandler -> send in AttackDefinition
         // combat handler will manage attack based on definition
+
+    public static void catchInput(Player player, Vec3 deltaMovement) {
+        Level level = player.level();
+        if(!level.isClientSide()) MainCombatHandler.punch(player, deltaMovement);
+    }
 }
