35563ba99127537db8ff349fc71c471aeef0a667
jakeinman05 <jakeinman05@gmail.com>
2026-06-19 12:58:55 -0400

    initial player interaction networking

--- src/main/java/net/poob22/normaldm/common/client/packet/PlayerLeftClickEmptyPacket.java
+++ src/main/java/net/poob22/normaldm/common/client/packet/PlayerLeftClickEmptyPacket.java
@@ -1,38 +1,43 @@
 package net.poob22.normaldm.common.client.packet;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkEvent;
+import net.poob22.normaldm.common.server.combat.LeftClickHandler;
 
 import java.util.UUID;
 import java.util.function.Supplier;
 
 public class PlayerLeftClickEmptyPacket {
     UUID playerUUID;
+    Vec3 deltaMovement;
 
-    public PlayerLeftClickEmptyPacket(UUID id) {
-        this.playerUUID = id;
+    public PlayerLeftClickEmptyPacket(UUID playerUUID, Vec3 deltaMovement) {
+        this.playerUUID = playerUUID;
+        this.deltaMovement = deltaMovement;
     }
 
     public static void encode(PlayerLeftClickEmptyPacket msg, FriendlyByteBuf buf) {
         buf.writeUUID(msg.playerUUID);
+        buf.writeDouble(msg.deltaMovement.x);
+        buf.writeDouble(msg.deltaMovement.y);
+        buf.writeDouble(msg.deltaMovement.z);
     }
 
     public static PlayerLeftClickEmptyPacket decode(FriendlyByteBuf buf) {
-        return new PlayerLeftClickEmptyPacket(buf.readUUID());
+        return new PlayerLeftClickEmptyPacket(buf.readUUID(), new Vec3(buf.readDouble(), buf.readDouble(), buf.readDouble()));
     }
 
     public static void handle(PlayerLeftClickEmptyPacket msg, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
-            Level level = Minecraft.getInstance().level;
-            if(level != null) {
-                Player player = level.getPlayerByUUID(msg.playerUUID);
-            }
-            else {
-                throw new NullPointerException("Level in PlayerLeftClickEmptyPacket is null");
-            }
+            Level level = ctx.get().getSender().level();
+            Player player = level.getPlayerByUUID(msg.playerUUID);
+
+            if(player == null) throw new NullPointerException("ServerPlayer on normaldm:CombatInputPacket is null");
+
+            LeftClickHandler.catchInput(player, msg.deltaMovement);
         });
     }
 }
