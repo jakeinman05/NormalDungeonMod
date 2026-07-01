5c0cbdd2d60f10b8beeed566b8820e935d3c6bba
jakeinman05 <jakeinman05@gmail.com>
2026-07-01 12:15:14 -0400

    debug gui draws player combat data

--- src/main/java/net/poob22/normaldm/common/client/render/gui/GuiOverlayRenderer.java
+++ src/main/java/net/poob22/normaldm/common/client/render/gui/GuiOverlayRenderer.java
@@ -3,16 +3,13 @@ package net.poob22.normaldm.common.client.render.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
-
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
+import net.poob22.normaldm.common.server.combat.capability.data.StatsDataComponent;
+import net.poob22.normaldm.common.server.combat.capability.data.stats.StatType;
 
 import static net.poob22.normaldm.common.server.combat.CombatUtil.calculateReach;
-import static net.poob22.normaldm.common.server.combat.capability.CombatInternalCapabilities.COMBAT_DATA_CAPABILITY;
+import static net.poob22.normaldm.common.server.combat.capability.CombatInternalCapabilities.COMBAT;
 
 public class GuiOverlayRenderer {
-    private static double GRAVITY_CONSTANT = 0.0784000015258789;
-
     public static void render(GuiGraphics gui) {
         Minecraft mc = Minecraft.getInstance();
         Player player = mc.player;
@@ -20,18 +17,18 @@ public class GuiOverlayRenderer {
         if(player != null) {
             gui.drawString(mc.font, "Combat Debug", 10, 126, 0XFFFFFF);
             gui.drawString(mc.font, "Reach: " + calculateReach(player, player.getDeltaMovement()), 10, 136, 0XFFFFFF);
-            gui.drawString(mc.font, "Capability Present: " + player.getCapability(COMBAT_DATA_CAPABILITY).isPresent(), 10, 146, 0XFFFFFF);
+            gui.drawString(mc.font, "Capability Present: " + player.getCapability(COMBAT).isPresent(), 10, 146, 0XFFFFFF);
 
-            AtomicInteger combos = new AtomicInteger();
-            AtomicLong comboTimer = new AtomicLong();
-            player.getCapability(COMBAT_DATA_CAPABILITY).ifPresent(c -> {
-                combos.set(c.getCombosData().getCombos());
-                comboTimer.set(c.getCombosData().getComboTimer());
+            player.getCapability(COMBAT).ifPresent(c -> {
+                gui.drawString(mc.font, "Combos: " + c.getCombosData().getCombos(), 10, 156, 0XFFFFFF);
+                gui.drawString(mc.font, "Combo Timer: " + Math.max(0, c.getCombosData().getComboTimer()), 10, 166, 0XFFFFFF);
+                gui.drawString(mc.font, "Cooldown: " + c.getCooldownData().getCooldown(), 10, 176, 0XFFFFFF);
+
+                StatsDataComponent stats = c.getStats();
+                for(int i = 0; i < StatType.values().length; i++) {
+                    gui.drawString(mc.font, StatType.values()[i].getSerializedName() + ": " + stats.getStat(StatType.values()[i]), 10, 186 + (i* 10), 0XFFFFFF);
+                }
             });
-            if(comboTimer.get() - Minecraft.getInstance().level.getGameTime() <= 0) combos.set(0);
-
-            gui.drawString(mc.font, "Combos: " + combos.get(), 10, 156, 0XFFFFFF);
-            gui.drawString(mc.font, "Combo Timer: " + Math.max(0, (comboTimer.get() - Minecraft.getInstance().level.getGameTime())), 10, 166, 0XFFFFFF);
         }
     }
 }
