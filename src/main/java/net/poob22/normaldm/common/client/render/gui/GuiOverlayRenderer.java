13067798ead301c01cb463c80d6f21161b6ac019
jakeinman05 <jakeinman05@gmail.com>
2026-07-02 13:40:16 -0400

    changed range to round off to 2 decimal

--- src/main/java/net/poob22/normaldm/common/client/render/gui/GuiOverlayRenderer.java
+++ src/main/java/net/poob22/normaldm/common/client/render/gui/GuiOverlayRenderer.java
@@ -18,7 +18,7 @@ public class GuiOverlayRenderer {
             player.getCapability(COMBAT).ifPresent(c -> {
                 if(c.showDebug()) {
                     gui.drawString(mc.font, "Combat Debug", 10, 126, 0XFFFFFF);
-                    gui.drawString(mc.font, "Reach: " + calculateReach(player, player.getDeltaMovement()), 10, 136, 0XFFFFFF);
+                    gui.drawString(mc.font, String.format("Reach: %.2f", calculateReach(player, player.getDeltaMovement())), 10, 136, 0XFFFFFF);
                     gui.drawString(mc.font, "Capability Present: " + player.getCapability(COMBAT).isPresent(), 10, 146, 0XFFFFFF);
 
                     gui.drawString(mc.font, "Combos: " + c.getCombosData().getCombos(), 10, 156, 0XFFFFFF);
